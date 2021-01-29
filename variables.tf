20f8c5b4384dc9ef52282613b64424a36990a2ce
willmenn <william.ahrons@gmail.com>
2021-01-29 10:41:32 +0100

    Add vars

--- variables.tf
+++ variables.tf
@@ -2,9 +2,16 @@ variable "service_name" {}
 variable "internal_port" {}
 variable "external_port" {}
 variable "port_protocol" {}
-
+variable "account_number" {}
+variable "cpu" {
+  default = 256
+}
+variable "memory" {
+  default = 256
+}
+variable "account_number" {}
 variable "region" {
-  default = "eu-west-1"
+  default = "sa-east-1"
 }
 
 variable "environment" {
